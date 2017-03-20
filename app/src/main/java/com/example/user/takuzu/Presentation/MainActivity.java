19bc8749730d320fccbc208c6326fd01169f5485
tomas rechtig <tomas.rechtig@7interactive.cz>
2017-03-20 15:21:21 +0100

    vomit

--- app/src/main/java/com/example/user/takuzu/Presentation/MainActivity.java
+++ app/src/main/java/com/example/user/takuzu/Presentation/MainActivity.java
@@ -2,14 +2,28 @@ package com.example.user.takuzu.Presentation;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 import com.example.user.takuzu.R;
 
+import butterknife.BindView;
+import butterknife.OnClick;
+
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.startGame)
+    TextView startGame;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+
+    @OnClick(R.id.startGame)
+    public void submit(View view){
+        System.out.println("AUTA");
     }
 }
