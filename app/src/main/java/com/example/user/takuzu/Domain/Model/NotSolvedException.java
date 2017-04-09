d1922d49982350a3288b44df810357b5a845499b
coolklokan <coolklokan@gmail.com>
2017-04-09 14:48:31 +0200

    commit

---
+++ app/src/main/java/com/example/user/takuzu/Domain/Model/NotSolvedException.java
@@ -0,0 +1,15 @@
+package com.example.user.takuzu.Domain.Model;
+
+/**
+ * Created by user on 08.04.2017.
+ */
+
+public class NotSolvedException extends Exception {
+
+    private NotSolvedReason reason;
+    private int line;
+
+    public NotSolvedException() {
+
+    }
+}
---
+++ app/src/main/java/com/example/user/takuzu/Domain/Model/NotSolvedReason.java
@@ -0,0 +1,9 @@
+package com.example.user.takuzu.Domain.Model;
+
+/**
+ * Created by user on 08.04.2017.
+ */
+
+public enum NotSolvedReason {
+
+}
